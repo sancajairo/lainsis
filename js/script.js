4992bddd2479d6d468634d0e71e7720526891720
John Sanchez <sancajairo@gmail.com>
2022-04-22 15:35:49 -0500

    Creando javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function sayHello(){
+    alert("Hola Amir");
+}
+function addMessage(){
+    var text= document.createElement("Amir");
+    text.textContent = "Maria Nimueh";
+    document.body.appendChild(text);
+}
