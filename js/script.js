906ea67d71a24e9477b515b6e45b5fa56be59618
John Sanchez <sancajairo@gmail.com>
2022-04-25 09:30:38 -0500

    Eliminando Js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function sayHello(){
-    alert("Hola Amir");
-}
-function addMessage(){
-    var text= document.createElement("Amir");
-    text.textContent = "Maria Nimueh";
-    document.body.appendChild(text);
-}
