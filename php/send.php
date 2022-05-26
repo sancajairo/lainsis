c66b01018d1074af1bb0d6f8686929bdfdc2314f
John Sanchez <sancajairo@gmail.com>
2022-05-26 15:07:42 -0500

    Formulario de Contacto

---
+++ php/send.php
@@ -0,0 +1,28 @@
+<?php   
+
+    $nombre = $_POST['nombre'];
+    $mail = $_POST['mail'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = 'From: ' . $mail . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+    
+    $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+    $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+    $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+    $mensaje .= "Enviado el " . date('d/m/Y', time());
+    
+    if(mail("correo, correo", "Mensaje de mi sitio web", $mensaje, $header))
+    {
+        echo "Mensaje enviado correctamente";
+    }
+    else
+    {
+        echo "Error al enviar el mensaje";
+    }
+
+    header("Location: index.html");
+    
+?>
