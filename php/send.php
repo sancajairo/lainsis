73e8b4bd8abb44100e2a2e97dc53232d47e2935c
John Sanchez <sancajairo@gmail.com>
2022-05-27 16:28:30 -0500

    Formulario de Contacto funcional

--- php/send.php
+++ php/send.php
@@ -1,28 +1,28 @@
 <?php   
 
     $nombre = $_POST['nombre'];
-    $mail = $_POST['mail'];
+    $asunto = $_POST['asunto'];
+    $correo = $_POST['correo'];
     $mensaje = $_POST['mensaje'];
 
-    $header = 'From: ' . $mail . " \r\n";
-    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header = 'From: ' . $correo . " \r\n"; 
+    $header .= "X-Mailer: PHP/".phpversion()." \r\n";
     $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
+
+    $mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+    $mensaje .= "Su e-mail es: " . $correo . " \r\n";
+    $mensaje .= "Mensaje: " . $mensaje . " \r\n";
     
-    $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-    $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-    $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-    $mensaje .= "Enviado el " . date('d/m/Y', time());
-    
-    if(mail("sancajairo@gmail.com", "Mensaje de mi sitio web", $mensaje, $header))
-    {
-        echo "Mensaje enviado correctamente";
-    }
-    else
-    {
-        echo "Error al enviar el mensaje";
-    }
+    $para = "sancajairo@gmail.com"; 
+    $asunto = "Mensaje desde Lainsis";  
+
+    mail($para, $asunto, $mensaje, $header);
+
+    echo "Mensaje enviado";
 
     header("Location: index.html");
-    
+
+    exit;
+
 ?>
