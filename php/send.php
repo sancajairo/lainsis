bc72e90f09ee61eaf55b6e24f3d1bdc30e2c73c7
John Sanchez <sancajairo@hotmail.com>
2022-05-30 20:09:22 -0500

    Whatsapp

--- php/send.php
+++ php/send.php
@@ -1,27 +1,26 @@
 <?php   
 
     $nombre = $_POST['nombre'];
-    $asunto = $_POST['asunto'];
     $correo = $_POST['correo'];
-    $mensaje = $_POST['mensaje'];
+    $message = $_POST['mensaje'];
 
-    $header = 'From: ' . $correo . " \r\n"; 
+   
     $header .= "X-Mailer: PHP/".phpversion()." \r\n";
     $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
 
-    $mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+    $mensaje .= "Este mensaje fue enviado por: " . $nombre . " \r\n";
     $mensaje .= "Su e-mail es: " . $correo . " \r\n";
-    $mensaje .= "Mensaje: " . $mensaje . " \r\n";
+    $mensaje .= "Atender el siguiente mensaje: " . $message . " \r\n";
     
-    $para = "sancajairo@gmail.com"; 
-    $asunto = "Mensaje desde Lainsis";  
+    $para = "laboratoriolainci@gmail.com"; 
+    $asunto = "Mensaje desde Lainsis Oficial";  
 
     mail($para, $asunto, $mensaje, $header);
 
     echo "Mensaje enviado";
 
-    header("Location: index.html");
+    
 
     exit;
 
